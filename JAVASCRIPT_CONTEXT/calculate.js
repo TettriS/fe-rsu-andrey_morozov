234beb6639f6e87daa38d8bc1611cb02ff0cc635
TettriS <resetter@mail.ru>
2016-11-29 23:42:14 +0300

    29.11.2016

--- JAVASCRIPT_CONTEXT/calculate.js
+++ JAVASCRIPT_CONTEXT/calculate.js
@@ -20,25 +20,10 @@ function Calculator () {
 	this.getResult = function () {
         return this.result;
     };
-    this.resetTo = function (newResult) {
-        this.result = newResult;
-        return this;
-    };
     this.reset = function () {
         this.result = 0;
         return this;
     };
-}
-// for (var prop in user) {
-//   if (typeof user[prop] == 'function') {
-//     user[prop] = user[prop].bind(user);
-//   }
-// }
-function getInitialState(callback) {
-    setTimeout(function () { 
-    callback(5);
-    console.log(calc.getResult());
-    }, 500);
 }
 var calc = new Calculator();
 console.log(calc.getResult());
@@ -49,6 +34,4 @@ calc.multiply(4);
 console.log(calc.getResult());
 calc.reset();
 console.log(calc.getResult());
-calc.add(4).reset().add(1).getResult();
-console.log(calc.getResult());
-getInitialState(calc.resetTo.bind(calc));
+calc.add(4).reset().add(1).getResult(); // 1
