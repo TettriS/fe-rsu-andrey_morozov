2b597f3616801a3452c10f3e092b668f69cfa939
TettriS <resetter@mail.ru>
2016-12-06 15:37:25 +0300

    first

---
+++ JAVASCRIPT_OOP,_MVC/MVC TASK/js/model.js
@@ -0,0 +1,130 @@
+function Model() {
+    this.books = [
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            name: 'Empty'
+        },
+        {
+            coverAddress : "style/images/10.png",
+            name : "Vegetables Cookbook",
+            author : "by Matthew Biggs", 
+            rating : 1
+        },
+        {
+            coverAddress : "style/images/9.png",
+            name : "Jamie's Italy",
+            author : "by Jamie Oliver", 
+            rating : 2
+        },
+        {
+            coverAddress : "style/images/8.png",
+            name : "Jamie Does",
+            author : "by Jamie Oliver", 
+            rating : 3
+        },
+        {
+            coverAddress : "style/images/7.png",
+            name : "Suneeta Vaswani",
+            author : "by Geeta Devi", 
+            rating : 5
+        },
+        {
+            coverAddress : "style/images/6.png",
+            name : "Cook Like an Italian",
+            author : "by Chrissy Gower", 
+            rating : 5
+        },
+        {
+            coverAddress : "style/images/5.png",
+            name : "Paleo Slow Cooking",
+            author : "by Chrissy Gower", 
+            rating : 5
+        },
+        {
+            coverAddress : "style/images/4.png",
+            name : "Inexpensive Family Meals",
+            author : "by Simon Holst", 
+            rating : 5
+        },
+        {
+            coverAddress : "style/images/3.png",
+            name : "Jamie's Kitchen",
+            author : "by Jamie Oliver", 
+            rating : 3
+        },
+        {
+            coverAddress : "style/images/2.png",
+            name : "Cakes & Bakes",
+            author : "by Sanjeev Kapoor", 
+            rating : 2
+        },
+        {
+            coverAddress : "style/images/1.png",
+            name : "Jewels of Nizam",
+            author : "by Geeta Devi", 
+            rating : 1
+        },
+    ];
+    this.notifications = [
+        {
+            did: "You added",
+            name: "",
+            author: "",
+            list: "",
+            time: ""
+        },
+        {
+            did: "You use filter",
+            name: "",
+            time: ""
+        },
+        {
+            did: "You use search",
+            time: ""
+        }
+    ];
+}
---
+++ JAVASCRIPT_OOP,_MVC/MVC TASK/js/view.js
@@ -0,0 +1,77 @@
+function View(model, controller) {
+    this.model = model;
+    this.ctrl = controller;
+    
+    // this.counterElement = document.getElementById('counter');
+    this.search = document.getElementById('search');
+    this.addBook = document.getElementById('addABook');
+}
+
+View.prototype.createBook = function(book) {
+    var bookBlock = document.querySelector(".books-block__list");
+
+    var bookCase = document.createElement('li');
+    bookCase.className = 'books-block__item';
+
+    var bookElem = document.createElement('ul');
+    bookElem.className = 'books-block__book';
+    bookCase.appendChild(bookElem);
+
+    if (book.name === 'Empty') {
+
+        var bookPartCover = document.createElement('li');
+        bookPartCover.className = 'books-block__book-part';
+        bookElem.appendChild(bookPartCover);
+
+    } else {
+
+        var bookPartCover = document.createElement('li');
+        bookPartCover.className = 'books-block__book-part';
+        bookElem.appendChild(bookPartCover);
+
+        var bookCover = document.createElement('img');
+        bookCover.setAttribute('src', book.coverAddress);
+        bookCover.setAttribute('alt', 'book cover');
+        bookCover.className = 'books-block--image';
+        bookPartCover.appendChild(bookCover);
+
+        var bookPartName = document.createElement('li');
+        bookPartName.className = 'books-block__book-part';
+        bookElem.appendChild(bookPartName);
+
+        var bookName = document.createElement('span');
+        bookName.className = 'books-block--name';
+        bookName.innerHTML = book.name;
+        bookPartName.appendChild(bookName);
+
+        var bookPartAuthor = document.createElement('li');
+        bookPartAuthor.className = 'books-block__book-part';
+        bookElem.appendChild(bookPartAuthor);
+
+        var bookAuthor = document.createElement('span');
+        bookAuthor.className = 'books-block--author';
+        bookAuthor.innerHTML = book.author;
+        bookPartAuthor.appendChild(bookAuthor);
+
+        var bookPartRating = document.createElement('li');
+        bookPartRating.className = 'books-block__book-part';
+        bookElem.appendChild(bookPartRating);
+
+        for (var i = 0; i < 5; i++) {
+            var bookRating = document.createElement('i');
+            if (i < book.rating) { 
+                bookRating.className = 'fa fa-star books-block--stars';
+            } else {
+                bookRating.className = 'fa fa-star-o books-block--stars';
+            }
+            bookPartRating.appendChild(bookRating);
+        }
+    }
+    bookBlock.appendChild(bookCase);
+}
+
+View.prototype.init = function() {
+    for (var i = this.model.books.length - 1; i >= 0; i--) {
+        this.view.createBook(this.model.books);
+    }
+}
